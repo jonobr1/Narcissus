ebd83ec9e237b13e38b5dc30c6da9ec616b55ef1
jonobr1 <jonobrandel@gmail.com>
2011-03-25 18:05:05 -0700

    updated source code

--- include/Resources.h
+++ include/Resources.h
@@ -1,4 +1,8 @@
 #pragma once
 #include "cinder/CinderResources.h"
 
-//#define RES_MY_RES			CINDER_RESOURCE( ../resources/, image_name.png, 128, IMAGE )
+#define RES_HALO				CINDER_RESOURCE( ../resources/, halo100.png, 128, IMAGE )
+#define RES_RECORD				CINDER_RESOURCE( ../resources/, record.png, 129, IMAGE )
+#define RES_IN_RECORD			CINDER_RESOURCE( ../resources/, recording.png, 130, IMAGE )
+#define RES_CHECK_DESK			CINDER_RESOURCE( ../resources/, checkDesktop.png, 131, IMAGE )
+#define RES_VERLAG				CINDER_RESOURCE( ../resources/, VerlagCondensedBook.ttf, 132, TTF )
